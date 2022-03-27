b237b883bfffa9b5275e9f6abdda554c9a32fbfe
Michael Osterhout <mikeosterhout@gmail.com>
2022-03-27 13:04:46 -0400

    make postgres default and no longer need version or schema env var

--- README.md
+++ README.md
@@ -6,16 +6,15 @@ Migration tool for rds databases
 
 `npm install rds-migrate`
 
-You will need the following environment variables:
+The following environment variables are used(some optional):
 
 ```
-RDS_MIGRATION_VERSION=0
+AWS_PROFILE=(optional) profile picked up by aws-sdk
 RDS_RESOURCE_ARN=Your RDS Cluster ARN
 RDS_SECRET_ARN=Your RDS Secret ARN
-RDS_DATABASE=Your RDS Database
-RDS_SCHEMA=Your RDS Schema
 RDS_REGION=Your RDS Region ex. us-east-2
-RDS_RDBMS=Default is "mysql". "postgres" also accepted
+RDS_RDBMS=(optional) Default is "postgres". "mysql" also accepted
+RDS_DATABASE=(optional)Your RDS Database. Will default to RDS_RDBMS
 ```
 
 Add to your package.json `scripts` section:
@@ -26,15 +25,13 @@ Add to your package.json `scripts` section:
 
 Where the argument passed(`src/migrations`) is the location of your migrations directory relative to package.json directory.
 
-### How it works
-
-The script will create a MIGRATIONS schema and a table matching your `RDS_SCHEMA` environment variable. The `RDS_SCHEMA` table will contain the current migration version. Running `rds-migrate` will check the table and run new migrations if needed.
-
 ### Prerequisites
 
-- All migration scripts should have numeric filenames starting at 0 and be placed in the directory passed to rds-migrate command. For example `/src/migrations/0.sql`, `/src/migrations/1.sql` etc.
+- All migration scripts should have numeric filenames and be placed in the directory passed to rds-migrate command. For example `/src/migrations/123.sql` or `/src/migrations/456_another_migration.sql` etc. The script will split the filename on underscores and use the first section to determine if new migrations are needed.
 
-- Before running each statement the `APP_SCHEMA_ENV` placeholder will be replaced by the `RDS_SCHEMA` environment variable. This allows for a dynamic schema.
+### How it works
+
+The script will create a `MIGRATIONS` schema with a single `version` table. Running `rds-migrate` will check the current version and run migrations if needed. For example, if the previous migration file that ran was `123_init.sql` the `version` column in the `MIGRATIONS.version` table will be `123_init.sql`. Subsequent `rds-migrate` runs will check for files with a number greater than `123` and run those in order. So if files `345_next.sql` and `678_another.sql` are added both of those will run and the new version stored will be `678_another.sql`.
 
 ## Running the tests
 
--- index.js
+++ index.js
@@ -6,18 +6,18 @@ const {
 const fs = require("fs");
 const chalk = require("chalk");
 
+const engine = process.env.RDS_RDBMS || "postgres";
+
 const rdsDataClient = new RDSDataClient({
   apiVersion: "2018-08-01",
   region: process.env.RDS_REGION,
-  database: process.env.RDS_DATABASE,
+  database: process.env.RDS_DATABASE || engine,
 });
 
 const params = {
   resourceArn: process.env.RDS_RESOURCE_ARN,
   secretArn: process.env.RDS_SECRET_ARN,
 };
-
-const engine = process.env.RDS_RDBMS || "mysql";
 
 const checkMigrationsSchema = async () => {
   try {
@@ -36,13 +36,13 @@ const checkMigrationsSchema = async () => {
         ...params,
         sql:
           engine === "postgres"
-            ? `CREATE TABLE IF NOT EXISTS MIGRATIONS.${process.env.RDS_SCHEMA} (
+            ? `CREATE TABLE IF NOT EXISTS MIGRATIONS.version (
               id SERIAL,
-              version INT NULL DEFAULT NULL,
+              version varchar NULL DEFAULT NULL,
               PRIMARY KEY (id))`
-            : `CREATE TABLE IF NOT EXISTS MIGRATIONS.${process.env.RDS_SCHEMA} (
+            : `CREATE TABLE IF NOT EXISTS MIGRATIONS.version (
               id INT NOT NULL AUTO_INCREMENT,
-              version INT NULL DEFAULT NULL,
+              version varchar NULL DEFAULT NULL,
               PRIMARY KEY (id))
               ENGINE = InnoDB`,
       })
@@ -52,144 +52,134 @@ const checkMigrationsSchema = async () => {
   }
 };
 
-var checkMigrationTable = async () => {
+const checkMigrationTable = async () => {
   try {
     // get the current version
     const { records } = await rdsDataClient.send(
       new ExecuteStatementCommand({
         ...params,
-        sql: `SELECT version FROM MIGRATIONS.${process.env.RDS_SCHEMA} WHERE id = 1`,
+        sql: `SELECT version FROM MIGRATIONS.version WHERE id = 1`,
       })
     );
-    // if we have a version
-    if (records.length === 1) {
-      var currentMigration = parseInt(records[0][0]["longValue"]);
-      var envMigration = parseInt(process.env.RDS_MIGRATION_VERSION);
-      if (currentMigration === envMigration) {
-        return {
-          needsMigrations: false,
-          message: `${chalk.yellow("Warn:")} Current migration ${chalk.magenta(
-            envMigration
-          )} is up to date`,
-        };
-      }
-      return {
-        needsMigrations: true,
-        currentMigration,
-      };
-      //if it's the first migration start at -1
-    } else if (parseInt(process.env.RDS_MIGRATION_VERSION) === 0) {
-      return {
-        needsMigrations: true,
-        currentMigration: -1,
-      };
-    }
-    throw new Error(
-      "No migration table and migration version is > 0. That is bad."
-    );
+    return records.length ? records[0][0]["stringValue"] : false;
   } catch (error) {
     throw new Error("check migration table failed", { cause: error });
   }
 };
 
-var migrate = async (currentMigration) => {
-  const executeStatements = async () => {
-    const envMigration = parseInt(process.env.RDS_MIGRATION_VERSION);
-    while (currentMigration < envMigration) {
-      currentMigration++;
+const updateSchemaMigrationVersion = async (fileName) => {
+  const data = await rdsDataClient.send(
+    new ExecuteStatementCommand({
+      ...params,
+      sql:
+        engine === "postgres"
+          ? `INSERT INTO MIGRATIONS.version (id, version)
+              VALUES (1, '${fileName}') ON CONFLICT (id) DO UPDATE SET version = excluded.version;`
+          : `INSERT INTO MIGRATIONS.version (id, version)
+              VALUES (1, '${fileName}') ON DUPLICATE KEY UPDATE version=VALUES(version);`,
+    })
+  );
+  console.log(
+    chalk(
+      chalk.blue("Info:"),
+      `Migration Complete`,
+      chalk.magenta(fileName),
+      chalk.gray(JSON.stringify(data))
+    )
+  );
+};
 
-      var filePath = `${process.env.PWD}/${
-        process.argv.slice(2)[0]
-      }/${currentMigration}.sql`;
-      if (!fs.existsSync(filePath)) {
-        throw new Error(`File does not exist ${filePath}`);
-      }
+const executeFileStatements = async (fileName) => {
+  const filePath = `${process.env.PWD}/${process.argv.slice(2)[0]}/${fileName}`;
+  if (!fs.existsSync(filePath)) {
+    throw new Error(`File does not exist ${filePath}`);
+  }
 
-      console.log(
-        chalk(
-          chalk.blue("Info:"),
-          `Running Migration`,
-          chalk.magenta(currentMigration)
-        )
-      );
-      var contents = fs.readFileSync(filePath, "utf8");
-      var contents = contents.replace(
-        /APP_SCHEMA_ENV/g,
-        process.env.RDS_SCHEMA
-      );
-      var statements = contents.split(";");
-      for (let i = 0; i < statements.length; i++) {
-        let sql = statements[i];
-        if (!sql || sql == "\n") {
-          continue;
-        }
-        console.log(
-          chalk(
-            chalk.blue("Info:"),
-            `Running Migration Statement`,
-            chalk.magenta(i + 1),
-            "\n",
-            sql,
-            "\n"
-          )
-        );
+  console.log(
+    chalk(chalk.blue("Info:"), `Running Migration`, chalk.magenta(fileName))
+  );
+  const contents = fs.readFileSync(filePath, "utf8");
+  const statements = contents.split(";");
 
-        const data = await rdsDataClient.send(
-          new ExecuteStatementCommand({
-            ...params,
-            sql,
-          })
-        );
-
-        console.log(
-          chalk(
-            chalk.blue("Info:"),
-            `Success Migration Statement`,
-            chalk.magenta(i + 1),
-            "\n",
-            chalk.gray(JSON.stringify(data)),
-            "\n\n"
-          )
-        );
-      }
+  for (let i = 0; i < statements.length; i++) {
+    let sql = statements[i];
+    if (!sql || sql == "\n") {
+      continue;
     }
-  };
-
-  const updateSchemaMigrationVersion = async () => {
-    const data = await rdsDataClient.send(
-      new ExecuteStatementCommand({
-        ...params,
-        sql:
-          engine === "postgres"
-            ? `INSERT INTO MIGRATIONS.${process.env.RDS_SCHEMA} (id, version)
-              VALUES (1, ${currentMigration}) ON CONFLICT (id) DO UPDATE SET version = excluded.version;`
-            : `INSERT INTO MIGRATIONS.${process.env.RDS_SCHEMA} (id, version)
-              VALUES (1, ${currentMigration}) ON DUPLICATE KEY UPDATE version=VALUES(version);`,
-      })
-    );
     console.log(
       chalk(
         chalk.blue("Info:"),
-        `Migration Complete`,
-        chalk.magenta(currentMigration),
-        chalk.gray(JSON.stringify(data))
+        `Running Migration Statement`,
+        chalk.magenta(i + 1),
+        "\n",
+        sql,
+        "\n"
       )
     );
-  };
 
-  await executeStatements();
-  await updateSchemaMigrationVersion();
+    const data = await rdsDataClient.send(
+      new ExecuteStatementCommand({
+        ...params,
+        sql,
+      })
+    );
+
+    console.log(
+      chalk(
+        chalk.blue("Info:"),
+        `Success Migration Statement`,
+        chalk.magenta(i + 1),
+        "\n",
+        chalk.gray(JSON.stringify(data)),
+        "\n\n"
+      )
+    );
+  }
+  await updateSchemaMigrationVersion(fileName);
+};
+
+const getFileNameTime = (fileName) => BigInt(fileName.split("_")[0]);
+
+const migrate = async (currentMigrationVersion) => {
+  const files = fs.readdirSync(
+    `${process.env.PWD}/${process.argv.slice(2)[0]}`
+  );
+
+  const currentMigrationFileTime = getFileNameTime(currentMigrationVersion);
+
+  const filteredFiles = files
+    .filter(
+      (file) =>
+        !currentMigrationVersion ||
+        getFileNameTime(file) > currentMigrationFileTime
+    )
+    .sort((a, b) =>
+      getFileNameTime(a) < getFileNameTime(b)
+        ? -1
+        : executeFileStatements(a) > executeFileStatements(b)
+        ? 1
+        : 0
+    );
+
+  if (!filteredFiles.length) {
+    console.log(
+      `${chalk.yellow("Warn:")} Current migration ${chalk.magenta(
+        currentMigrationVersion
+      )} is up to date`
+    );
+    return;
+  }
+
+  for (const file of filteredFiles) {
+    await executeFileStatements(file);
+  }
 };
 
 const run = async () => {
   try {
     await checkMigrationsSchema();
-    const { needsMigrations, message, currentMigration } =
-      await checkMigrationTable();
-    if (!needsMigrations) {
-      return console.log(message);
-    }
-    await migrate(currentMigration);
+    const currentMigrationVersion = await checkMigrationTable();
+    await migrate(currentMigrationVersion);
   } catch (error) {
     console.log(chalk.red(error));
   }
